3a2f15bf29a3efc630938ddbbda1c5477b47aac8
Rachel Mak <ryc.mak@gmail.com>
2020-12-16 23:20:38 +1100

    Completed exercise 1b

--- 2020/day01/reportRepair.py
+++ 2020/day01/reportRepair.py
@@ -8,7 +8,7 @@ for num in file:
   nums.append(int(num))
 file.close()
 
-nums = np.array(nums)
+nums_array = np.array(nums)
 
 # Method 1 for finding two numbers that sum to 2020
 def find_nums1(nums_array):
@@ -17,6 +17,10 @@ def find_nums1(nums_array):
       if nums_array[i] + nums_array[j] == 2020:
         print("i: %i, num: %i" % (i, nums_array[i]))
         print("j: %i, num: %i" % (j, nums_array[j]))
+        print("product: ", nums_array[i] * nums_array[j])
+
+print("Running Method 1:")
+find_nums1(nums_array)
 
 # Time Method 1
 test1 = '''
@@ -34,6 +38,10 @@ def find_nums2(nums_array):
     if len(match) > 0:
       print("i: %i, num: %i" % (i, nums_array[i]))
       print("match: ", match)
+      print("product: ", [nums_array[i] * j for j in match])
+
+print("Running Method 2:")
+find_nums2(nums_array)
 
 # Time Method 2
 test2 = '''
@@ -42,3 +50,14 @@ def test2():
 '''
 time2 = timeit.timeit(test2)
 print("time2: %f" % time2)
+
+
+# Second part: finding three numbers summing to 2020
+for i in range(len(nums)):
+  for j in range(i + 1, len((nums))):
+    for k in range(j + 1, len((nums))):
+      if nums[i] + nums[j] + nums[k] == 2020:
+        print("i: %i, num: %i" % (i, nums[i]))
+        print("j: %i, num: %i" % (j, nums[j]))
+        print("k: %i, num: %i" % (k, nums[k]))
+        print("product: ", nums[i] * nums[j] * nums[k])
