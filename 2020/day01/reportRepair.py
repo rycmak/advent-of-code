a6aba2d8a6b2af9191d0e4364d8a2f0d3ac7df9d
Rachel Mak <ryc.mak@gmail.com>
2020-12-16 20:55:52 +1100

    Timing two different methods

--- 2020/day01/reportRepair.py
+++ 2020/day01/reportRepair.py
@@ -1,4 +1,5 @@
 import numpy as np
+import timeit
 
 file = open("input.txt", "r")
 nums = []
@@ -8,8 +9,36 @@ for num in file:
 file.close()
 
 nums = np.array(nums)
-for i in range(len(nums)):
-  for j in range(i, len((nums))):
-    if nums[i] + nums[j] == 2020:
-      print("i: %i, num: %i" % (i, nums[i]))
-      print("j: %i, num: %i" % (j, nums[j]))
+
+# Method 1 for finding two numbers that sum to 2020
+def find_nums1(nums_array):
+  for i in range(len(nums_array)):
+    for j in range(i, len((nums_array))):
+      if nums_array[i] + nums_array[j] == 2020:
+        print("i: %i, num: %i" % (i, nums_array[i]))
+        print("j: %i, num: %i" % (j, nums_array[j]))
+
+# Time Method 1
+test1 = '''
+def test1():
+  return find_nums1(nums)
+'''
+time1 = timeit.timeit(stmt=test1)
+print("time1: %f" % time1)
+
+
+# Method 2 for finding two numbers that sum to 2020
+def find_nums2(nums_array):
+  for i in range(len(nums_array)):
+    match = [y for y in nums_array[(i + 1):] if y + nums_array[i] == 2020]
+    if len(match) > 0:
+      print("i: %i, num: %i" % (i, nums_array[i]))
+      print("match: ", match)
+
+# Time Method 2
+test2 = '''
+def test2():
+  return find_nums2(nums)
+'''
+time2 = timeit.timeit(test2)
+print("time2: %f" % time2)
