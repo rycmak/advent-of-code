590ad0144647d8bff5c26447315d25daf2c4bb67
Rachel Mak <ryc.mak@gmail.com>
2020-12-17 00:38:29 +1100

    Completed exercise 2a

---
+++ 2020/day02/password_philosopy.py
@@ -0,0 +1,19 @@
+file = open("input.txt", "r")
+
+num_valid = 0
+
+for line in file:
+  # policy = part before colon
+  policy = line.strip().split(":")[0]
+  # get min/max number allowed for given letter
+  min_max = policy.split(" ")[0]
+  letter = policy.split(" ")[1]
+  min = int(min_max.split("-")[0])
+  max = int(min_max.split("-")[1])
+  
+  # password = part after colon
+  password = line.strip().split(":")[1]
+  if password.count(letter) >= min and password.count(letter) <= max:
+    num_valid += 1
+
+print("Number of valid passwords = ", num_valid)
