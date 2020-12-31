3a28039f121d3566bcd7ca59fed2fe267699205b
Rachel Mak <ryc.mak@gmail.com>
2020-12-31 14:52:33 +1100

    Completed exercise 4b

--- 2020/day04/passport_processing_b.py
+++ 2020/day04/passport_processing_b.py
@@ -1,3 +1,5 @@
+import re
+
 all_passports = []
 num_valid_passports = 0
 
@@ -10,7 +12,29 @@ def enter_passport_info(key_value_pairs, passport):
 
 def valid_passport(passport):
   required_keys = ['byr', 'eyr', 'ecl', 'hcl', 'hgt', 'iyr', 'pid']
-  return all(key in passport.keys() for key in required_keys)
+  if all(key in passport.keys() for key in required_keys):
+    # check each key contains valid value
+    return all(valid_value(key, value) for key, value in passport.items())
+
+def valid_value(key, value):
+  if key == 'byr':
+    return (int(value) >= 1920 and int(value) <= 2002)
+  elif key == 'iyr':
+    return (int(value) >= 2010 and int(value) <= 2020)
+  elif key == 'eyr':
+    return (int(value) >= 2020 and int(value) <= 2030)
+  elif key == 'hgt':
+    return (value[-2:] == "cm" and int(value[:-2]) >= 150 and int(value[:-2]) <= 193) or \
+           (value[-2:] == "in" and int(value[:-2]) >= 59 and int(value[:-2]) <= 76)
+  elif key == 'hcl':
+    return re.search('^#[0-9a-f]{6}$', value)
+  elif key == 'ecl':
+    return value in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+  elif key == 'pid':
+    return re.search('^\d{9}$', value)
+  elif key == 'cid':
+    return True
+
 
 # Run script
 file = open("input.txt", "r")
