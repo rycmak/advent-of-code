fa30c7099d7d8cd4f41e559418efd4fa9e02525c
Rachel Mak <ryc.mak@gmail.com>
2021-01-03 03:05:34 +1100

    Completed exercise 8a

---
+++ 2020/day08/handheld_halting.py
@@ -0,0 +1,47 @@
+# Find value of accumulator after following a series of steps
+#
+# Turn each line in input to a list where:
+# Index 0: tuple(instruction type (acc, jmp, or nop), integer argument)
+# Index 1: contains boolean visited for whether this instruction has already been run
+# OR
+# Index 1: array containing step number(s) which runs this instruction
+# Push this list into a super-list (of lists) called boot_code
+# 
+# Initialize accumulator = 0
+# Initialize boot_code_index = 0 (which boot code line currently running)
+# Start from first index in boot_code
+# Before running each instruction, check if visited
+# If visited, stop and return accumulater
+# Otherwise:
+# Set visited = True for instruction
+# If instruction type == acc, then increment accumulator by argument, 
+#   and go to next list (increment boot_code_line)
+# If instruction type == jmp, then increment boot_code_line by argument
+# If instruction type == nop, then increment boot_code_line by 1
+
+boot_code = []
+file = open("input.txt", "r")
+for line in file:
+  boot_code.append(line.strip().split())
+  # QUESTION: why does boot_code.append(line.strip().split().append(False)) not work???
+  # Instead, now we have to create a separate for loop below...
+
+for instruction in boot_code:
+  instruction.append(False)
+
+accumulator = 0
+boot_code_index = 0
+while True:
+  visited = boot_code[boot_code_index][2]
+  if visited: break
+  boot_code[boot_code_index][2] = True
+  instruction = boot_code[boot_code_index][0]
+  if instruction == "acc":
+    accumulator += int(boot_code[boot_code_index][1])
+    boot_code_index += 1
+  elif instruction == "jmp":
+    boot_code_index += int(boot_code[boot_code_index][1])
+  else:  # instruction == "nop"
+    boot_code_index += 1
+
+print("accumulator = ", accumulator)
