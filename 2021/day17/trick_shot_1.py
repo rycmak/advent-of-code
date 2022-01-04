a8a484ccbf471f0ce51b966388183ca6c2e4d5c0
Rachel Mak <ryc.mak@gmail.com>
2022-01-04 15:32:54 +1300

    Completed day 17

--- 2021/day17/trick_shot_1.py
+++ 2021/day17/trick_shot_1.py
@@ -2,7 +2,7 @@
 # then it will eventually fall back down to y=0 at velocity
 # -(vy + 1).  For the next downward step to not overshoot 
 # the target area, the max y velocity downward should be 
-# vy + 1 = abs(target_y_min)
+# -(vy + 1) = -abs(target_y_min)
 # i.e., vy = abs(target_y_min) - 1
 
 # Launched upward at vy = abs(target_y_min) - 1
