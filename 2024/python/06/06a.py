eb7abbafb5995f01be82add574a8a68da3b27693
rachel.mak <rachel.mak@pushpay.com>
2024-12-09 21:01:24 +1300

    Python day 6 part 1

---
+++ 2024/python/06/06a.py
@@ -0,0 +1,59 @@
+input = []
+inputCopy = []
+
+with open("input.txt") as f:
+  row = 0
+  for line in f:
+    if line.find('^') != -1:
+      currentPos = (row, line.find('^'))
+    input.append(list(line.strip()))
+    inputCopy.append(list(line.strip()))
+    row += 1
+
+numRows = len(input)
+numCols = len(input[0])
+inputCopy[currentPos[0]][currentPos[1]] = 'X'
+
+directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+currentDirectionIndex = 0  # current direction to move is (-1, 0)
+
+def getNextDirectionIndex():
+  global currentDirectionIndex
+  nextDirectionIndex = (currentDirectionIndex + 1) % 4  # there are 4 possible directions
+  currentDirectionIndex = nextDirectionIndex
+  return currentDirectionIndex
+
+def getNextPos(currentPos, currentDirection):
+  # print(f"currentPos = {currentPos}, currentDirection = {currentDirection}")
+  return tuple(map(lambda x, y: x + y, currentPos, currentDirection))
+
+def isValid(nextPos):
+  # print(f"isValid: nextPos = {nextPos}")
+  if nextPos[0] >=0 and nextPos[0] < numRows \
+    and nextPos[1] >= 0 and nextPos[1] < numCols:
+    return True
+  # print("not valid")
+  return False
+
+
+nextPos = getNextPos(currentPos, directions[currentDirectionIndex])
+
+while isValid(nextPos):
+  # print(f"while, currentPos = {currentPos}, char = {input[currentPos[0]][currentPos[1]]}")
+  if input[nextPos[0]][nextPos[1]] == '.' or input[nextPos[0]][nextPos[1]] == '^':
+    currentPos = nextPos
+    inputCopy[currentPos[0]][currentPos[1]] = 'X'
+    nextPos = getNextPos(currentPos, directions[currentDirectionIndex])
+    # print(f"After ., nextPos = {nextPos}")
+  elif input[nextPos[0]][nextPos[1]] == '#':
+    nextPos = getNextPos(currentPos, directions[getNextDirectionIndex()])
+    # print(f"After #, nextPos = {nextPos}")
+
+count = 0
+
+for i in range(numRows):
+  for j in range(numCols):
+    if inputCopy[i][j] == 'X':
+      count += 1
+
+print(count)
